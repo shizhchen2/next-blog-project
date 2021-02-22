35cd91617b149e6d9b542663a02f8ddb70c341b9
陈仕臻 <shizhenchen93@gmail.com>
2021-02-23 01:14:25 +0800

    adjustment

--- server/app/controller/hello.js
+++ server/app/controller/hello.js
@@ -2,10 +2,12 @@
 
 const Controller = require('egg').Controller;
 
-class HelloController extends ControllerP{
+class HelloController extends Controller{
     async index(){
         const {ctx} = this
         ctx.status = 200
         ctx.body = 'hello eggjs';
     }
 }
+
+module.exports = HelloController
