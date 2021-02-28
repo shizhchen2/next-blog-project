bd568803fa205ba5d055c39e2e2f12966eec1d63
陈仕臻 <shizhenchen93@gmail.com>
2021-02-28 17:53:51 +0800

    final lock

--- server/app/router.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * @param {Egg.Application} app - egg application
- */
-module.exports = app => {
-  require('./router/hello')(app)
-  require('./router/test')(app);
-};
--- server/app/router/test.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-    app.router.get('/test/get_example/:example_key', app.controller.test.getExample);
-    app.router.post('/test/set_example/:example_key/:example_value', app.controller.test.setExample);
-  };
-  
